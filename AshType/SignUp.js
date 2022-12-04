25b7c141ebf586dfce007612d8d0ce98c899859f
Ashish Antil <ashishantil.antil07@gmail.com>
2022-12-04 16:24:26 +0530

    fixed bugs

--- AshType/SignUp.js
+++
@@ -1,7 +0,0 @@
-const imageInput = document.getElementById("profImage");
-const userNameInput = document.getElementById("userNameTaker");
-
-const actionFun = () => {
-  localStorage.setItem("uname", userNameInput.value);
-  localStorage.setItem("picURL", imageInput.value);
-}
